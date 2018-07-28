798eed2d9b2a078e496477754401448e74480fb5
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-28 18:57:39 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
 
   patch '/posts/:id' do
     binding.pry
-      #@post = Post.find(params[:id])
+    @post = Post.find(params[:id])
     @post.name= params[:name]
     @post.content= params[:content]
     @post.save
